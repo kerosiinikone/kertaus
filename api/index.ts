9119101bd255e159eebc6bb76ad46172e277f67f
kerosiinikone <onni.ahopelto@gmail.com>
2023-06-30 13:11:13 +0300

    Refactor API index

--- api/index.ts
+++ api/index.ts
@@ -15,55 +15,64 @@ import { ScheduleResolver } from "./src/models/Schedule/resolver.ts";
 import { AuthResolver } from "./src/models/User/auth/authResolver.ts";
 import { UserResolver } from "./src/models/User/userResolver.ts";
 import { refreshRouter } from "./src/routes/refresh.ts";
+
 const { json } = pkg;
 dotenv.config();
 
 const main = async () => {
-  const schema = await buildSchema({
-    resolvers: [UserResolver, AuthResolver, ScheduleResolver],
-  });
-
-  await startPrisma();
   const app = express();
   const httpServer = http.createServer(app);
-
-  const client = new ApolloServer<ContextType>({
-    schema,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
+  const client = await buildApollo(httpServer);
 
   await client.start();
+  await startPrisma();
 
-  app.use(cookieParser());
-  app.use(
-    "/graphql",
-    cors<cors.CorsRequest>({
-      credentials: true,
-      origin: [process.env.CLIENT_URL],
-    }),
-    json(),
-    expressMiddleware(client, {
-      context: async ({ res, req }) => ({
-        req,
-        res,
+  app
+    .use(cookieParser())
+    .use(
+      "/graphql",
+      cors<cors.CorsRequest>({
+        credentials: true,
+        origin: [process.env.CLIENT_URL],
       }),
-    })
-  );
-
-  app.use(
-    "/refresh",
-    cors<cors.CorsRequest>({
-      credentials: true,
-      origin: [process.env.CLIENT_URL, "http://localhost:3000/me"],
-    })
-  );
-  app.use("/refresh", refreshRouter);
+      json(),
+      expressMiddleware(client, {
+        context: async ({ res, req }) => ({
+          req,
+          res,
+        }),
+      })
+    )
+    .use(
+      "/refresh",
+      cors<cors.CorsRequest>({
+        credentials: true,
+        origin: [process.env.CLIENT_URL, "http://localhost:3000/me"],
+      })
+    )
+    .use("/refresh", refreshRouter);
 
   await new Promise<void>((resolve) =>
     httpServer.listen({ port: process.env.PORT }, resolve)
   );
+
   if (process.env.ENVIRONMENT === "DEVELOPMENT")
     console.log(`🚀 Server ready at ${process.env.SERVER_URL}`);
+};
+
+const buildApollo = async (
+  httpServer: http.Server<
+    typeof http.IncomingMessage,
+    typeof http.ServerResponse
+  >
+) => {
+  const schema = await buildSchema({
+    resolvers: [UserResolver, AuthResolver, ScheduleResolver],
+  });
+  return new ApolloServer<ContextType>({
+    schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
 };
 
 await main();
