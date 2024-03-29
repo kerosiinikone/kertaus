93937910492da33f22b07023b85582b9f97f296d
kerosiinikone <onni.ahopelto@gmail.com>
2024-03-29 12:57:48 +0200

    Fixed test configs

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  test: {
+    exclude: [
+      "./client/e2e/**",
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*",
+    ],
+  },
+});
